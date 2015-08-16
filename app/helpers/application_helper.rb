f024bb010d0222c243633c5490cca66b1979ef8c
bkw795 <bkw795@gmail.com>
2015-08-16 03:04:01 +0000

    Added user sign up form, tests

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+
+  def full_title(page_title = '')
+    base_title = "Odyssey"
+    if page_title.empty?
+      base_title
+    else
+      "#{page_title} | #{base_title}"
+    end
+  end
+:w
+
 end
